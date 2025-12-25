49ff0c967eae6b727e1c41af21f52eb761c0f77c
Clark Wimberly <clark@clarklab.net>
2025-12-25 01:38:53 -0600

    width

--- demo/src/components/sections/hero-left-aligned-with-demo.tsx
+++ demo/src/components/sections/hero-left-aligned-with-demo.tsx
@@ -27,7 +27,7 @@ export function HeroLeftAlignedWithDemo({
         <div className="flex flex-col gap-32">
           <div className="flex flex-col items-start gap-6">
             {eyebrow}
-            <Heading className="max-w-5xl">{headline}</Heading>
+            <Heading className="max-w-7xl">{headline}</Heading>
             <Text size="lg" className="flex max-w-3xl flex-col gap-4">
               {subheadline}
             </Text>
